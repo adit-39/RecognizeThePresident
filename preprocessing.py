ceed8b885acb008e820bbc3183600dfb11b9242b
adit-39 <adit.39@gmail.com>
2015-03-16 14:54:12 +0530

    Preprocessing catering to wavfiles

--- preprocessing.py
+++ preprocessing.py
@@ -35,14 +35,14 @@ def get_audio_from_video(url,wavfile="temp"):
 	os.system(cmd)
 # conversion of audio file format: ffmpeg -i audio.aac audio.wav
 
-def kmeans_Mfcc(wavfile="temp.wav",opfile="temp_vq.txt"):
+def kmeans_Mfcc(codebook,wavfile="temp.wav",opfile="temp_vq.txt"):
 	"""
 	Obtain the MFCC coefficients from a wav file as a numpy matrix, then cluster the
 	attributes into a single numeric value and print into a file given by opfile
 	"""
 	rate,sig = wvf.read(wavfile)
 	mfcc_feat = mfcc(sig,rate)
-	codebook = sp.kmeans(mfcc_feat, 8)[0]
+	#codebook = sp.kmeans(mfcc_feat, 8)[0]
 	data = sp.vq(mfcc_feat,codebook)
 	f = open(opfile,"w")
 	for i in data[0]:
