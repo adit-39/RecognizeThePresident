bd4d2f21e5b1aab6ba6c91a65540eef66cb6de4f
adit-39 <adit.39@gmail.com>
2015-03-16 14:51:33 +0530

    Generating codebook

---
+++ create_training.py
@@ -0,0 +1,33 @@
+import scipy.io.wavfile as wvf
+import scipy.cluster.vq as sp
+import numpy as np
+import os
+import re
+import sys
+from features import mfcc
+
+def kmeans_Mfcc_mod_train():
+	"""
+	Obtain the MFCC coefficients from a wav file as a numpy matrix, then cluster the
+	attributes into a single numeric value and print into a file given by opfile
+	"""
+	rate,sig = wvf.read("all_trng.wav")
+	mfcc_feat = mfcc(sig,rate)
+	global codebook
+	codebook = sp.kmeans(mfcc_feat, 8)[0]
+	wavfiles = ["obama_trng.wav","other_trng.wav"]
+	for wavfile in wavfiles:
+		final = []
+		rate,sig = wvf.read(wavfile)
+		mfcc_feat = mfcc(sig,rate)
+		data = sp.vq(mfcc_feat,codebook)
+		for i in data[0]:
+			final.append(i)
+		f = open(wavfile.split(".")[0]+"_vq.txt","w")
+		for i in final:
+			f.write(str(i)+"\n")
+		f.close()
+	return codebook
+
+if __name__=="__main__":
+	kmeans_Mfcc_mod_train()
